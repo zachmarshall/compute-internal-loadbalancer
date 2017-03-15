c89e9052aa25d15de95cf72c3c7021cf35b5df70
Zach Marshall <Zach.Marshall@sas.com>
2017-03-14 20:14:23 -0400

    Discover instance groups members dynamically to form consul cluster
    Use compute metadata to determine instance group name, then use that
    instance group name to lookup the group name members. Use those
    member names as the consul server list.

--- images/consul/consul_servers.sh
+++ images/consul/consul_servers.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-in=$(curl -s -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/consul_servers)
-IFS=',' read -a servers <<< "$in"
-args="CONSUL_SERVERS=-bootstrap-expect=${#servers[@]} "
-#for x in "${servers[@]}"; do args+="-retry-join=$x "; done
-args+="-retry-join=${servers[0]}"
+# update gcloud sdk tool
+gcloud components update -q
+INSTANCE_GROUP_NAME=$(curl -s -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/instance_group_name)
+ZONE=$(curl -s -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/zone | sed 's:.*/::')
+INSTANCES=($(gcloud --format='value(instance)' compute instance-groups list-instances $INSTANCE_GROUP_NAME --zone=$ZONE))
+args="CONSUL_SERVERS=-bootstrap-expect=${#INSTANCES[@]} "
+for x in "${INSTANCES[@]}"; do args+="-retry-join=$x "; done
+#args+="-retry-join=${servers[0]}"
 echo $args
