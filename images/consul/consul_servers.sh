5cc5a84f41d95fd03cca376b1e51762bc566afa2
Zach Marshall <Zach.Marshall@sas.com>
2017-03-15 14:58:08 -0400

    Working cluster list
    However, images don't join each other immediately when they start up.
    They need to get an external IP address before they are able to join.

--- images/consul/consul_servers.sh
+++ images/consul/consul_servers.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # update gcloud sdk tool
-gcloud components update -q
+RETRY_COUNT=0
+until [ `gcloud components update -q` ] || [ "$RETRY_COUNT" -gt 12 ]; do
+    /usr/bin/systemd-cat -t "consul_servers" /usr/bin/echo "Could not update gcloud components"
+    sleep 5
+    ((RETRY_COUNT++))
+done
 INSTANCE_GROUP_NAME=$(curl -s -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/instance_group_name)
 ZONE=$(curl -s -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/zone | sed 's:.*/::')
 INSTANCES=($(gcloud --format='value(instance)' compute instance-groups list-instances $INSTANCE_GROUP_NAME --zone=$ZONE))
